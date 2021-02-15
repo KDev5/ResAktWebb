d54722f78db81139bd0cc27760884825edbbe1e1
Kevin Karlsson <keka0008@student.hv.se>
2021-02-15 16:18:01 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ResAktWebb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
