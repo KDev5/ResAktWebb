949c13a908fbe8b1fbe30ec484c7ffa3a008ae0d
antno <antno@LAPTOP-9U0B3ID2>
2021-02-16 14:25:11 +0100

    Controllers

---
+++ Data/ResAktWebbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ResAktWebb.Models;
+
+namespace ResAktWebb.Data
+{
+    public class ResAktWebbContext : DbContext
+    {
+        public ResAktWebbContext (DbContextOptions<ResAktWebbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ResAktWebb.Models.Reservation> Reservation { get; set; }
+
+        public DbSet<ResAktWebb.Models.Menu> Menu { get; set; }
+    }
+}
