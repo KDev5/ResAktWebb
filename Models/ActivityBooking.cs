608434ef098ddda89b3b07d5ce2575ecf4ff6166
Tobias <tobias.granbom@gmail.com>
2021-03-15 15:05:01 +0100

    översatt mer till svenska

--- Models/ActivityBooking.cs
+++ Models/ActivityBooking.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -11,7 +12,9 @@ namespace ResAktWebb.Models
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("Kundnamn")]
         public string CustomerName { get; set; }
+        [DisplayName("Antal deltagare")]
         public int NumParticipants { get; set; }
 
         //bör lägga till email och telefonnummer då vi inte har någon koppling till gästsidan.
@@ -19,7 +22,9 @@ namespace ResAktWebb.Models
         // FK
         
         [ForeignKey("Activity")]
+        [DisplayName("Aktivitet")]
 		public int ActivityId { get; set; }
+        [DisplayName("Aktivitet")]
         public virtual Activity Activity { get; set; }
 
         
